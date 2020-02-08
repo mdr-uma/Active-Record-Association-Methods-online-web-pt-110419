cef2c7a3f90a675c7b219553d7e3ad8576b40551
Uma Manandhar <um_2003_8@hotmail.com>
2020-02-08 19:49:15 +0000

    Automatically backed up by Learn

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -14,6 +14,6 @@ class Genre < ActiveRecord::Base
 
   def all_artist_names
     # return an array of strings containing every musician's name
-    self.class.all.artists
+    self.class.all.artist
   end
 end
